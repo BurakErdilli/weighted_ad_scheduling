4e35046e9465aeb3c35458044b72067f57d5f9d9
BurakErdilli <84309897+BurakErdilli@users.noreply.github.com>
2022-03-26 01:53:45 +0300

    all files added, recursive function will be fixed next

--- cmake-build-debug/CMakeFiles/max_profit.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/max_profit.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/berdi/CLionProjects/max_profit/main.c" "C:/Users/berdi/CLionProjects/max_profit/cmake-build-debug/CMakeFiles/max_profit.dir/main.c.obj"
+  "C:/Users/berdi/CLionProjects/cityGraph/main.c" "C:/Users/berdi/CLionProjects/cityGraph/cmake-build-debug/CMakeFiles/max_profit.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- main.c
+++ main.c
@@ -1,126 +1,235 @@
-
-#define MAX 500
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define N 30
 
-struct Advertisement {
+typedef struct Graph{
+    struct Node* head[N];
+}GRAPH;
+
+typedef struct Node{
+    char destination[15];
+    struct Node* next;
+}NODE;
+
+typedef struct Edge {
+    char source[15];
+    char destination[15];
     int id;
-    int StartTime;
-    int Duration;
-    int Profit;
-};
-int max(int a, int b);
-int findMaxProfit(struct Advertisement A[], int n);
-int cmpfunc(const void *a, const void *b);
-int collisionsBinarySearchCheck(struct Advertisement Ads[], int index);
+    int hour;
+    int minute;
+    int price;
+    int src_id;
+    int dest_id;
+}EDGE;
 
-int collisionsBinarySearchCheck(struct Advertisement Ads[], int index) {
-    int lo = 0;
-    int hi = index - 1;
-    int mid;
+typedef struct City {
+    int id;
+    char name[15];
+}CITY;
 
-    while (lo <= hi) {
-        mid = (lo + hi) / 2;
-        if ((Ads[mid].StartTime + Ads[mid].Duration) <= (Ads[index].StartTime)) {
-            if ((Ads[mid + 1].StartTime + Ads[mid + 1].Duration) <= (Ads[index].StartTime)) {
-                lo = mid + 1;
-            } else {
-                return mid;
+int dosyadanOku(EDGE*);
+int sehirID(EDGE*, CITY*, int);
+GRAPH* createGraph(EDGE*, int);
+void printGraph(GRAPH*, int);
+//int ucusListele(EDGE *edges, int, int, int, int ,int[20][20], int, int);
+
+int ucusListele(EDGE *edges, int n, int k, int s_id, int d_id, int rapor[20][20], int x, int y){
+
+    int i = 0;
+    int j = 0;
+    while(i < n && k > 0){
+        if(edges[i].src_id == s_id){
+            rapor[x][y] = edges[i].id;
+            printf("%d-> ", rapor[x][y]);
+            if(edges[i].dest_id == d_id){
+                x = 0;
+                return y++;
+            }else{
+                y = ucusListele(edges, n, k-1, edges[i].dest_id, d_id, rapor[20][20], x+1, y);
             }
-        } else {
-            hi = mid - 1;
         }
+        i++;
     }
-    return -1;
-}
-
-int cmpfunc(const void *a, const void *b) {
-    struct Advertisement *p_a;
-    struct Advertisement *p_b;
-    p_a = (struct Advertisement *) a;
-    p_b = (struct Advertisement *) b;
-    return (p_a->StartTime + p_a->Duration - (p_b->StartTime + p_b->Duration));
-}
-
-int max(int a, int b) {
-    return a > b ? a : b;
-}
-
-int findMaxProfit(struct Advertisement *A, int n) {
-    qsort(A, n, sizeof(struct Advertisement), cmpfunc);
-    int table[n];
-    int i, l, inclProf, result, j;
-    int data[n][n];
-    int index_count[n];
-    int check;
-    int index;
-    table[0] = A[0].Profit;
-    index_count[0] = 1;
-    data[0][0] = A[0].id;
-
-    for (i = 1; i < n; i++) {
-        check = 0;
-        inclProf = A[i].Profit;
-        l = collisionsBinarySearchCheck(A, i);
-        if (l != -1) {
-            inclProf += table[l];
-            check = 1;
-        }
-
-        if (check != 1) {
-            index_count[i] = 1;
-            data[i][0] = A[i].id;
-        }
-        else {
-
-            for (j = 0; j < index_count[l]; j++) {
-                data[i][j] = data[l][j];
+    if(k == 0){
+        while(i < n){
+            if(edges[i].src_id == s_id && edges[i].dest_id == d_id ){
+                rapor[x][y] = edges[i].id;
+                printf("%d-> ", rapor[x][y]);
+                return y++;
+            }else{
+                i++;
             }
-            data[i][j] = A[i].id;
-            index_count[i] = j + 1;
         }
+        return y;
+    }
+}
 
-        table[i] = max(inclProf, table[i - 1]);
-        if (table[i] == inclProf) {
-            index = i;
-        }
+int main(void){
+
+    int i=0, j;
+    int n;
+    int city_number;
+
+    EDGE edges[50];
+
+    n = dosyadanOku(edges);
+    CITY cities[2*n];
+
+    city_number = sehirID(edges, cities, n);
+    GRAPH *graph = createGraph(edges, n);
+
+    printf("\nSehirler ve ID'leri (Isim-ID)\n----------------------------\n");
+    for(i = 0; i < city_number; i++){
+        printf("%s-%d \n",
+               cities[i].name, cities[i].id);
     }
 
-    result = table[i - 1];
-    printf("\nAdvertisement indexes that are chosen: ");
-    for (i = 0; i < index_count[index]; i++) {
-        printf("%d ", data[index][i]);
+    printf("\nUcuslar ve Bilgileri\n-------------------------\n");
+    for (i = 0; i < n; i++) {
+        printf("%d No'lu Ucus --->"
+               "(%s, %s, %d saat,  %d dakika, %d tl, %d nolu sehirden, %d nolu sehire)  \n", i + 1,
+               edges[i].source, edges[i].destination, edges[i].hour,
+               edges[i].minute,edges[i].price,
+               edges[i].src_id,edges[i].dest_id);
     }
-    return result;
+
+    printGraph(graph, n);
+
+     int k = 2;
+     int s_id=0;
+     int d_id=2;
+     int x=0, y=0;
+     int rapor[n][n];
+     ucusListele(edges, n, k, s_id, d_id, rapor, x, y);
+     for(i = 0; i < n; i++){
+         for(j = 0; j < n; j++){
+             if(rapor[j][i] != NULL){
+                 printf("%s ",edges[rapor[j][i]].destination);
+             }
+         }
+
+     }
+
+    return 0;
 }
 
 
+int dosyadanOku(EDGE *edges){
 
-int main() {
-    struct Advertisement A[MAX];
+    int i = 0;
     char filename[20] = "Sample.txt";
     FILE *fp;
     fp = fopen(filename, "r");
-    int i = 0;
-    int j;
-    if (fp == NULL) {
-        printf("unable to open the file");
-        return -1;
-    } else {
-        printf("All the data in the file->> %s  \n\n", filename);
-        while (3 == fscanf(fp, "%d %d %d", &A[i].StartTime, &A[i].Duration, &A[i].Profit)) {
-            printf("Advertisement %d ---> (StartTime ,Duration ,Profit)  (%d , %d ,%d)  \n", i + 1, A[i].StartTime,
-                   A[i].Duration, A[i].Profit);
-            A[i].id = i + 1;
+    if (fp != NULL){
+        while (5 == fscanf(fp, "%s %s %d %d %d ", &edges[i].source, &edges[i].destination, &edges[i].hour,&edges[i].minute ,&edges[i].price)) {
+            edges[i].id = i;
             i++;
         }
+        return i;
+    }else{
+        printf("\nDosya acilamadi!");
         fclose(fp);
-        printf("\n\nSorting by finishTimes.. \n\n");
-        qsort(A, i, sizeof(struct Advertisement), cmpfunc);
-        for (j = 0; j < i; ++j) {
-            printf("id:%d \t%d \t%d \t%d \n", A[j].id, A[j].StartTime, A[j].Duration, A[j].Profit);
-        }
-        printf("\n\nMaximum profit: %d.\n ", findMaxProfit(A, i));
-        return 0;
+        free(fp);
+        return -1;
     }
 }
+
+
+int sehirID(EDGE *edges, CITY *cities , int n){
+
+    int i = 0;
+    int j = 0;
+    int check = 0;
+    int count1 = 1;
+    int count2 = 2;
+
+    strcpy(cities[0].name, edges[0].source);
+    strcpy(cities[1].name, edges[0].destination);
+
+    for(i=0; i < 2; i++)
+        cities[i].id = i;
+
+    edges[0].src_id=0;
+    edges[0].dest_id=1;
+    for(i = 1; i < n; i++){
+        while(j < count2){
+            if(strcmp(cities[j].name, edges[count1].source) == 0){
+                check = 1;
+                edges[count1].src_id = cities[j].id;
+                j = count2;
+            }
+            j++;
+        }
+        if(check == 0){
+            strcpy(cities[count2].name, edges[count1].source);
+            cities[count2].id = count2;
+            edges[count1].src_id = cities[count2].id;
+            count2++;
+        }
+        count1++;
+        check = 0;
+        j = 0;
+    }
+    count1 = 1;
+    for(i = 1; i < n; i++){
+        while(j < count2){
+            if(strcmp(cities[j].name, edges[count1].destination) == 0){
+                check = 1;
+                edges[count1].dest_id = cities[j].id;
+                j = count2;
+            }
+            j++;
+        }
+        if(check == 0){
+            strcpy(cities[count2].name, edges[count1].destination);
+            cities[count2].id = count2;
+            edges[count1].dest_id = cities[count2].id;
+            count2++;
+        }
+        count1++;
+        check = 0;
+        j = 0;
+    }
+    return count2;
+}
+
+
+GRAPH* createGraph(EDGE edges[], int n){
+    int i;
+    char source[15];
+    char destination[15];
+    GRAPH* graph = (GRAPH*)malloc(sizeof(GRAPH));
+
+    for (i = 0; i < n; i++) {
+        graph->head[i] = NULL;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        strcpy(source, edges[i].source);
+        strcpy(destination, edges[i].destination);
+
+        NODE* newNode = (NODE*)malloc(sizeof(NODE));
+        strcpy(newNode->destination, destination);
+
+        newNode->next = graph->head[edges[i].src_id];
+
+        graph->head[edges[i].src_id] = newNode;
+    }
+
+    return graph;
+}
+
+void printGraph(GRAPH* graph, int n){
+    int i;
+    for (i = 0; i < n; i++){
+        NODE* ptr = graph->head[i];
+        while (ptr != NULL){
+            printf("\n(%d nolu sehirden-> %s)", i, ptr->destination);
+            ptr = ptr->next;
+        }
+        printf("\n");
+    }
+}
+
